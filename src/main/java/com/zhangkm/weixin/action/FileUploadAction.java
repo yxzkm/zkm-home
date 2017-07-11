e48fff7dee9762d05dd9a1c463e42e88526daadd
yxzkm <yxzkm@163.com>
2017-07-11 08:58:21 +0800

    新增关闭应用的get方法

--- src/main/java/com/zhangkm/weixin/action/FileUploadAction.java
+++ src/main/java/com/zhangkm/weixin/action/FileUploadAction.java
@@ -30,14 +30,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.zhangkm.weixin.base.ReturnData;
 import com.zhangkm.weixin.service.MediaService;
 import com.zhangkm.weixin.service.MetaDataService;
 import com.zhangkm.weixin.service.RedisService;
 
-import io.swagger.annotations.Api;
 import net.coobird.thumbnailator.Thumbnails;
 import net.coobird.thumbnailator.Thumbnails.Builder;
 import net.coobird.thumbnailator.geometry.Positions;
@@ -324,51 +322,6 @@ public class FileUploadAction {
 		return new ReturnData(0);  
     }  
     
-    /**
-     * 多文件上传
-     * 注意html中form和input的写法，input的name为：filesSelected，要和后端对应
-     * @param request
-     * @return
-     */
-    @RequestMapping(value="/multiFilesUpload", method=RequestMethod.POST)
-   	@ResponseBody
-   	public ReturnData multiFilesUpload(MultipartHttpServletRequest request){
-    			
-   		List<MultipartFile> files = ((MultipartHttpServletRequest) request).getFiles("filesSelected");
-    		
-   		if (files==null) return new ReturnData(-1);
-   		if (files.size()==0) return new ReturnData(-1);
-
-   		for(MultipartFile file : files){
-
-   			if (file.isEmpty()) return new ReturnData(-1);
-    			
-   			logger.info("MultipartFile.getName():"+file.getName());
-   			logger.info("MultipartFile.getOriginalFilename():"+file.getOriginalFilename());
-   			
-			String serverFullFileName = UPLOAD_FILE_SAVE_PATH + UUID.randomUUID().toString() + ".jpg";
-
-			byte[] bytes = null;
-			FileOutputStream fos = null;
-					
-			try {
-				bytes = file.getBytes();
-				fos = new FileOutputStream(serverFullFileName); 
-				fos.write(bytes); 
-			} catch (Exception e) {
-				e.printStackTrace();
-				return new ReturnData(-1);
-			} finally {
-				try {
-					fos.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-   		}
-   		return new ReturnData(0);
-   	}
-
     private String getPostStringFromRequest(HttpServletRequest request){
     	InputStream inputStream = null;
 		ByteArrayOutputStream baos = null;
